8ca0ec5551b03a0df7498882b25651460b93ad8f
kalindir <kalindir.lunar@gmail.com>
2025-02-24 01:10:08 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AppsMenuConsole;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunApp();
+    }
+
+    static void RunApp()
+    {
+        ConsoleUI.ShowMenu();
+        ConsoleUI.DisplayApps();
+        ConsoleUI.ChooseApp();
+        RunApp();
+    }
+}
